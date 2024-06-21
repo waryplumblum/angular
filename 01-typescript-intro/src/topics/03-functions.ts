2a1d6f4af0b074051b2e81dc7030d205abd37a81
Eduardo H. García <91917354+waryplumblum@users.noreply.github.com>
2024-06-21 15:06:21 -0600

    feat: Functions with objects as arguments

--- 01-typescript-intro/src/topics/03-functions.ts
+++ 01-typescript-intro/src/topics/03-functions.ts
@@ -11,12 +11,33 @@ function multiply( firstNumber:number , secondNumber?:number , base:number = 2){
     return firstNumber * base;
 }
 
+// const result: number = addNumbers(1,2);
+// const result2: string = addNumbersArrow(1,2);
+// const result3: number = multiply(5);
+// console.log({result,result2,result3});
 
-const result: number = addNumbers(1,2);
-const result2: string = addNumbersArrow(1,2);
-const result3: number = multiply(5);
+interface Character{
+    name: string;
+    hp: number;
+    showHp: () => void;
+}
 
+const healCharacter = ( character:Character , amount:number) => {
+    character.hp += amount;
+}
 
-console.log({result,result2,result3});
+const strider: Character = {
+    name: 'Strider',
+    hp: 50,
+    showHp(){
+        console.log(`Puntos de vida ${this.hp}`);
+    }
+}
+
+healCharacter(strider,10);
+healCharacter(strider,50);
+
+strider.showHp();
+
 
 export{};
