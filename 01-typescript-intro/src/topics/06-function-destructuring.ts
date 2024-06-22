3bc563b307c5360dd87e4d84cb924bf275f29f71
Eduardo H. García <91917354+waryplumblum@users.noreply.github.com>
2024-06-21 18:05:02 -0600

    feat: Destructuring of arguments

---
+++ 01-typescript-intro/src/topics/06-function-destructuring.ts
@@ -0,0 +1,45 @@
+
+
+interface Product {
+    description: string;
+    price: number;
+}
+
+const phone: Product = {
+    description: 'Nokia A1',
+    price: 150.0
+}
+
+const tablet: Product = {
+    description: 'iPad Air',
+    price: 250.0
+}
+
+interface TaxCalculationOptions{
+    tax: number;
+    products: Product[];
+}
+
+function taxCalculation( options:TaxCalculationOptions ): number[]{
+    let total = 0;
+
+    options.products.forEach( product => {
+        total += product.price;
+    });
+    return [total, total * options.tax];
+}
+
+const shoppingCart = [ phone , tablet ];
+const tax = 0.15;
+
+const result = taxCalculation({
+    products: shoppingCart,
+    tax,
+});
+
+console.log('Total', result[0]);
+console.log('Tax', result[1]);
+
+// * La tarea es aplicar la destructuracion de todo el ejercicio.
+
+export{};
