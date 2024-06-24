8c60ca3dac343e4692dd490e9fe9fb064f124f32
Eduardo H. García <91917354+waryplumblum@users.noreply.github.com>
2024-06-24 17:13:42 -0600

    feat: forms to depure the app

--- 02-bases/src/app/dbz/components/add-character/add-character.component.ts
+++ 02-bases/src/app/dbz/components/add-character/add-character.component.ts
@@ -17,13 +17,12 @@ export class AddCharacterComponent {
   }
 
   emitCharacter():void {
-    console.log(this.character);
+    // debugger;
     if ( this.character.name.length === 0 )  return;
 
     this.onNewCharacter.emit(this.character);
 
-    this.character.name = '';
-    this.character.power = 0;
+    this.character = { name: '', power: 0 };
   }
 
 }
