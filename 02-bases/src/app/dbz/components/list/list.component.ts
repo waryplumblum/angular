dd91d8a698b0608f8c0c7e2ce8ecd3beeee329eb
Eduardo H. García <91917354+waryplumblum@users.noreply.github.com>
2024-06-24 14:57:14 -0600

    feat: @input - receive from the father

--- 02-bases/src/app/dbz/components/list/list.component.ts
+++ 02-bases/src/app/dbz/components/list/list.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
   selector: 'app-dbz-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
-export class ListComponent implements OnInit {
+export class ListComponent {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  @Input()
+  public characterList: Character[] = [{
+    name: 'Trunks',
+    power: 10
+  }];
 
 }
