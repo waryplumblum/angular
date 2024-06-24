a11dfa255e2ddbfcf72c77516adc40aa9c70b1b7
Eduardo H. García <91917354+waryplumblum@users.noreply.github.com>
2024-06-24 14:05:01 -0600

    feat: dbz list component

--- 02-bases/src/app/dbz/dbz.module.ts
+++ 02-bases/src/app/dbz/dbz.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MainPageComponent } from './pages/main-page.component';
+import { ListComponent } from './components/list/list.component';
 
 
 
 @NgModule({
   declarations: [
     MainPageComponent,
+    ListComponent
   ],
   exports: [
     MainPageComponent,
