25baca5ab4aece9094ca3c1ccab7f1300dcbddef
Eduardo H. García <91917354+waryplumblum@users.noreply.github.com>
2024-06-23 23:27:46 -0600

    feat: one way data binding

--- 02-bases/src/app/heroes/hero/hero.component.ts
+++ 02-bases/src/app/heroes/hero/hero.component.ts
@@ -10,4 +10,12 @@ export class HeroComponent {
   public name: string = 'ironman';
   public age: number = 45;
 
+  get capitalizedName():string {
+    return this.name.toUpperCase();
+  }
+
+  getHeroDescription():string {
+    return `${ this.name } - ${ this.age }`;
+  }
+
 }
